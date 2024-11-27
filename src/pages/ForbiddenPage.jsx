2069b9425b34fd694d894c9a66f4371d8e52ff2e
Trinh Hoai Duc <anhduc08768@gmail.com>
2024-11-28 04:55:22 +0700

    create 403 404 pages

--- src/pages/ForbiddenPage.jsx
+++ src/pages/ForbiddenPage.jsx
@@ -1,4 +1,20 @@
+import { Button } from '@/components/ui/button';
+import { Link } from 'react-router';
+
 const ForbiddenPage = () => {
-  return <div>ForbiddenPage</div>;
+  return (
+    <div className='flex min-h-screen flex-col items-center justify-center py-20 text-center'>
+      <img src='/images/403.png' alt='' className='w-1/4' />
+      <div className='mx-auto mt-12 w-full max-w-[560px]'>
+        <h4 className='mb-4 text-4xl font-semibold text-slate-900'>Không có quyền truy cập</h4>
+        <div className='mb-10 text-base text-gray-500'>
+          Bạn không có quyền truy cập vào trang này. Vui lòng liên hệ quản trị viên.
+        </div>
+      </div>
+      <Button size='lg' asChild>
+        <Link to='/'>Trang chủ</Link>
+      </Button>
+    </div>
+  );
 };
 export default ForbiddenPage;
--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -1,4 +1,20 @@
+import { Button } from '@/components/ui/button';
+import { Link } from 'react-router';
+
 const NotFoundPage = () => {
-  return <div>NotFoundPage</div>;
+  return (
+    <div className='flex min-h-screen flex-col items-center justify-center py-20 text-center'>
+      <img src='/images/404.png' alt='' className='w-1/4' />
+      <div className='mx-auto mt-12 w-full max-w-[560px]'>
+        <h4 className='mb-4 text-4xl font-semibold text-slate-900'>Không tìm thấy</h4>
+        <div className='mb-10 text-base text-gray-500'>
+          Trang bạn đang tìm kiếm có thể đã bị xóa do đã thay đổi tên hoặc tạm thời không khả dụng.
+        </div>
+      </div>
+      <Button size='lg' asChild>
+        <Link to='/'>Trang chủ</Link>
+      </Button>
+    </div>
+  );
 };
 export default NotFoundPage;
