3f92ddcfd066b9ec8b9d91577e15086af85ce47e
Trinh Hoai Duc <anhduc08768@gmail.com>
2024-11-29 03:37:02 +0700

    create breadcrumb

--- src/components/layouts/AdminLayout/AdminSidebar/AdminSidebarHeader.jsx
+++ src/components/layouts/AdminLayout/AdminSidebar/AdminSidebarHeader.jsx
@@ -6,6 +6,7 @@ import {
   SidebarMenuItem,
   useSidebar
 } from '@/components/ui/sidebar';
+import { Link } from 'react-router';
 
 export default function AdminSidebarHeader() {
   const { state } = useSidebar();
@@ -16,12 +17,14 @@ export default function AdminSidebarHeader() {
           size='lg'
           className='data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground'
         >
-          <div className='text-sidebar-primary-foreground flex aspect-square items-center justify-center rounded-lg'>
-            {state === 'collapsed' ? (
-              <img src='/images/logo.png' alt='' />
-            ) : (
-              <img src='/images/brand-logo.png' alt='' />
-            )}
+          <div className='flex aspect-square items-center justify-center rounded-lg text-sidebar-primary-foreground'>
+            <Link to='/'>
+              {state === 'collapsed' ? (
+                <img src='/images/logo.png' alt='' />
+              ) : (
+                <img src='/images/brand-logo.png' alt='' />
+              )}
+            </Link>
           </div>
         </SidebarMenuButton>
       </SidebarMenuItem>
--- src/components/layouts/AdminLayout/index.jsx
+++ src/components/layouts/AdminLayout/index.jsx
@@ -1,15 +1,8 @@
-import {
-  Breadcrumb,
-  BreadcrumbItem,
-  BreadcrumbLink,
-  BreadcrumbList,
-  BreadcrumbPage,
-  BreadcrumbSeparator
-} from '@/components/ui/breadcrumb';
 import { Separator } from '@/components/ui/separator';
 import { SidebarInset, SidebarProvider, SidebarTrigger } from '@/components/ui/sidebar';
 import { Outlet } from 'react-router';
 import { AdminSidebar } from './AdminSidebar';
+import AdminBreadcrumb from './AdminBreadcrumb';
 
 function AdminLayout() {
   return (
@@ -20,17 +13,7 @@ function AdminLayout() {
           <div className='flex items-center gap-2 px-4'>
             <SidebarTrigger className='-ml-1' />
             <Separator orientation='vertical' className='mr-2 h-4' />
-            <Breadcrumb>
-              <BreadcrumbList>
-                <BreadcrumbItem className='hidden md:block'>
-                  <BreadcrumbLink href='#'>Building Your Application</BreadcrumbLink>
-                </BreadcrumbItem>
-                <BreadcrumbSeparator className='hidden md:block' />
-                <BreadcrumbItem>
-                  <BreadcrumbPage>Data Fetching</BreadcrumbPage>
-                </BreadcrumbItem>
-              </BreadcrumbList>
-            </Breadcrumb>
+            <AdminBreadcrumb />
           </div>
         </header>
         <div className='flex flex-1 flex-col gap-4 p-4 pt-0'>
