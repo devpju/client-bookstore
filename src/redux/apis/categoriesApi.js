e2ea269a5d34725e4bade4b07608f5a38e9d6650
Trinh Hoai Duc <anhduc08768@gmail.com>
2024-12-04 01:40:07 +0700

    handle refresh data when create

--- src/redux/apis/categoriesApi.js
+++ src/redux/apis/categoriesApi.js
@@ -4,18 +4,21 @@ import { createApi } from '@reduxjs/toolkit/query/react';
 export const categoriesApi = createApi({
   reducerPath: 'categoriesApi',
   baseQuery: baseQueryWithReauth,
+  tagTypes: ['Categories'],
   endpoints: (builder) => ({
     getFullCategories: builder.query({
       query: () => ({
         url: '/admin/categories'
-      })
+      }),
+      providesTags: ['Categories']
     }),
     createNewCategory: builder.mutation({
       query: ({ name }) => ({
         url: '/admin/categories',
         method: 'POST',
         body: { name }
-      })
+      }),
+      invalidatesTags: ['Categories']
     })
   })
 });
