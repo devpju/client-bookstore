aafe6e13cc0bc76bdb02729a9af8befdf352f805
Trinh Hoai Duc <anhduc08768@gmail.com>
2024-12-04 17:45:46 +0700

    create usersApI

---
+++ src/redux/apis/usersApi.js
@@ -0,0 +1,44 @@
+import baseQueryWithReauth from '@/config/baseQuery';
+import { createApi } from '@reduxjs/toolkit/query/react';
+
+export const usersApi = createApi({
+  reducerPath: 'usersApi',
+  baseQuery: baseQueryWithReauth,
+  tagTypes: ['Users'],
+  endpoints: (builder) => ({
+    fetchUsers: builder.query({
+      query: () => ({
+        url: '/admin/users'
+      }),
+      providesTags: ['Users']
+    }),
+    fetchDetailUsers: builder.query({
+      query: ({ id }) => ({
+        url: `/admin/users/${id}`
+      })
+    }),
+    editUserRoles: builder.mutation({
+      query: ({ id, roles }) => ({
+        url: `/admin/users/${id}`,
+        method: 'PUT',
+        body: { roles }
+      }),
+      invalidatesTags: ['Users']
+    }),
+    removeUsers: builder.mutation({
+      query: ({ userIds }) => ({
+        url: '/admin/users',
+        method: 'DELETE',
+        body: { userIds }
+      }),
+      invalidatesTags: ['Users']
+    })
+  })
+});
+
+export const {
+  useFetchDetailUsersQuery,
+  useFetchUsersQuery,
+  useEditUserRolesMutation,
+  useRemoveUsersMutation
+} = usersApi;
